619dcb0cd589419da66b1ede92a0d07861e919c0
Jesse Wattenbarger <jesse.j.wattenbarger@gmail.com>
2019-10-31 11:11:47 -0400

    Add bubblesort to ocaml.

--- ocaml/scratch/src/scratch.ml
+++ ocaml/scratch/src/scratch.ml
@@ -10,3 +10,17 @@ let rec insert lst x =
 (* let insertion_sort = List.fold_left insert [];; *)
 (* Core.Std List.fold_left needs named args *)
 let insertion_sort = List.fold_left ~f:insert ~init:[];;
+
+(* bubblesort *)
+let rec bubblesort s =
+  let rec _bubbler = function
+    | x :: x2 :: xs when x > x2 ->
+      x2 :: _bubbler (x :: xs)
+    | x :: x2 :: xs ->
+      x :: _bubbler (x2 :: xs)
+    | s -> s
+  in
+  let t = _bubbler s in
+  if t = s then t
+  else bubblesort t
+
