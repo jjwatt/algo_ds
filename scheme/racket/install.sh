f5bb355796de944e00c9857a121a9ae89f552588
Jesse Wattenbarger <jesse.j.wattenbarger@gmail.com>
2019-11-01 19:46:05 -0400

    Add reasoned schemer submodule and adjust install.sh.

--- scheme/racket/install.sh
+++ scheme/racket/install.sh
@@ -5,8 +5,8 @@ chmod +x racket-7.4-x86_64-linux.sh
 ./racket-7.4-x86_64-linux.sh
 rm ./racket-7.4-x86_64-linux.sh
 export PATH=$HOME/racket/bin:$PATH
-raco pkg install minikanren
-raco pkg install pie
-raco pkg install dracula
+# raco pkg install minikanren
+# raco pkg install pie
+# raco pkg install dracula
 raco pkg install --auto beautiful-racket
 
