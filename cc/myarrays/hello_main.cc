c7983b0f5144adb4db891a79ec103acabb8eeb09
Jesse Wattenbarger <jwatt@broken.watch>
2020-03-30 15:56:21 -0400

    Fix hello_main.cc.

--- cc/myarrays/hello_main.cc
+++ cc/myarrays/hello_main.cc
@@ -3,6 +3,6 @@
 #include <iostream>
 
 int main(int argc, const char** argv) {
-  std::cout << hello:Greet(argc < 2 ? "world" : argv[1]) << std::endl;
+  std::cout << hello::Greet(argc < 2 ? "world" : argv[1]) << std::endl;
   return 0;
 }
