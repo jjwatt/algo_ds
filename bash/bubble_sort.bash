b2037c62de395cba97207003fdfcd8e409369c8c
Jesse Wattenbarger <jwatt@broken.watch>
2025-04-13 07:42:02 -0400

    Working swap() and bubblesort()

--- bash/bubble_sort.bash
+++ bash/bubble_sort.bash
@@ -1,23 +1,43 @@
-#!/bin/env bash
+#/usr/bin/env bash
 
 swap() {
-    local arr_name="$1"
-    local index1="$2"
-    local index2="$3"
+  local -n arr="$1"
+  local index1="$2"
+  local index2="$3"
 
-    local temp="${!arr_name[$index1]}"
-    eval "${arr_name}[$index1]=\"${!arr_name[$index2]}\""
-    eval "${arr_name}[$index2]=\"$temp\""
+  local temp="${arr[$index1]}"
+  arr[$index1]="${arr[$index2]}"
+  arr[$index2]="$temp"
 }
 
+bubblesort() {
+  local ar=("${@}")
+  declare -n arr_ref="ar"
+  local len="${#ar[@]}"
+  local swapped
 
+  for ((i=0; i < len - 1; i++)); do
+    swapped=0
+    for ((j=0; j < len - i - 1; j++)); do
+      local current="${arr_ref[$j]}"
+      local next="${arr_ref[$((j + 1))]}"
+      if [[ "$current" > "$next" ]]; then
+        swap "arr_ref" "$j" "$((j + 1))"
+        swapped=1
+      fi
+    done
+    if ((swapped == 0)); then
+      break
+    fi
+  done
 
-mylist=("a" "b")
+  printf "%s " "${arr_ref[@]}"
+  echo ""
+}
 
-printf "%s\n" "${mylist[@]}"
+main() {
+    local mylist=(e d c b a)
+    bubblesort "${mylist[@]}"
+}
 
-swap mylist 0 1
-
-printf "%s\n" "${mylist[@]}"
-
-
+main "$@"
