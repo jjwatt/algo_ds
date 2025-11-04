a40444dd0012102560910002b2da314ffdf42f57
Jesse Wattenbarger <jwatt@broken.watch>
2025-11-04 15:41:39 -0500

    clang-format

--- c/linked_list.c
+++ c/linked_list.c
@@ -1,24 +1,24 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 typedef int item_type;
 
 typedef struct Node {
     item_type data;
-    struct Node *next;
+    struct Node* next;
 } Node;
 
 typedef struct LinkedList {
-    Node *head;
-    Node *tail;
+    Node* head;
+    Node* tail;
     size_t size;
 } LinkedList;
 
 static Node* create_node(item_type data) {
-    Node *new_node = (Node *)malloc(sizeof(Node));
+    Node* new_node = (Node*)malloc(sizeof(Node));
     if (new_node == NULL) {
-	return NULL;
+        return NULL;
     }
     new_node->data = data;
     new_node->next = NULL;
@@ -26,9 +26,9 @@ static Node* create_node(item_type data) {
 }
 
 LinkedList* list_create() {
-    LinkedList *list = (LinkedList *)malloc(sizeof(LinkedList));
+    LinkedList* list = (LinkedList*)malloc(sizeof(LinkedList));
     if (list == NULL) {
-	return NULL;
+        return NULL;
     }
     list->head = NULL;
     list->tail = NULL;
@@ -36,76 +36,77 @@ LinkedList* list_create() {
     return list;
 }
 
-void list_free(LinkedList *list) {
-    if (list == NULL) return;
+void list_free(LinkedList* list) {
+    if (list == NULL)
+        return;
     Node* current = list->head;
-    Node *next;
+    Node* next;
     while (current != NULL) {
-	next = current->next;
+        next = current->next;
         free(current);
-	current = next;
+        current = next;
     }
     free(list);
 }
 
-void list_print(const LinkedList *list) {
+void list_print(const LinkedList* list) {
     if (list == NULL) {
-	printf("List is (null)\n");
+        printf("List is (null)\n");
     }
-    Node *current = list->head;
+    Node* current = list->head;
     printf("List (size %zu): HEAD -> ", list->size);
     while (current != NULL) {
-	printf("[%d] -> ", current->data);
-	current = current->next;
+        printf("[%d] -> ", current->data);
+        current = current->next;
     }
     printf("NULL\n");
 }
 
-bool list_push_front(LinkedList *list, item_type data) {
+bool list_push_front(LinkedList* list, item_type data) {
     if (list == NULL)
-	return false;
+        return false;
     Node* new_node = create_node(data);
     if (new_node == NULL)
-	return false;
+        return false;
     if (list->head == NULL) {
-	list->head = new_node;
-	list->tail = new_node;
+        list->head = new_node;
+        list->tail = new_node;
     } else {
-	new_node->next = list->head;
-	list->head = new_node;
+        new_node->next = list->head;
+        list->head = new_node;
     }
     list->size++;
     return true;
 }
 
-bool list_append(LinkedList *list, item_type data) {
+bool list_append(LinkedList* list, item_type data) {
     if (list == NULL)
-	return false;
-    Node *new_node = create_node(data);
+        return false;
+    Node* new_node = create_node(data);
     if (new_node == NULL)
-	return false;
+        return false;
     if (list->head == NULL) {
-	list->head = new_node;
+        list->head = new_node;
         list->tail = new_node;
     } else {
-	list->tail->next = new_node;
+        list->tail->next = new_node;
         list->tail = new_node;
     }
     list->size++;
     return true;
 }
 
-bool list_pop_front(LinkedList *list, item_type *result) {
+bool list_pop_front(LinkedList* list, item_type* result) {
     if (list == NULL || list->head == NULL) {
-	return false;
+        return false;
     }
-    Node *temp = list->head;
+    Node* temp = list->head;
     *result = temp->data;
 
     list->head = list->head->next;
 
     if (list->head == NULL) {
-	list->tail = NULL;
+        list->tail = NULL;
     }
 
     free(temp);
@@ -113,9 +114,33 @@ bool list_pop_front(LinkedList *list, item_type *result) {
     return true;
 }
 
+bool list_delete_value(LinkedList* list, item_type data) {
+    if (list == NULL || list->head == NULL)
+        return false;
+    Node* current = list->head;
+    Node* prev = NULL;
+    while (current != NULL && current->data != data) {
+        prev = current;
+        current = current->next;
+    }
+    if (current == NULL)
+        return false;
+    if (prev == NULL) {
+        list->head = current->next;
+    } else {
+        prev->next = current->next;
+    }
+    if (current->next == NULL) {
+        list->tail = prev;
+    }
+    free(current);
+    list->size--;
+    return true;
+}
+
 int main() {
     printf("Creating list...\n");
-    LinkedList *mylist = list_create();
+    LinkedList* mylist = list_create();
     list_append(mylist, 10);
     list_append(mylist, 20);
     list_append(mylist, 30);
@@ -123,8 +148,8 @@ int main() {
     printf("Popping front...\n");
     item_type popped_val;
     if (list_pop_front(mylist, &popped_val)) {
-	printf("Popped: %d\n", popped_val);
-    }      
+        printf("Popped: %d\n", popped_val);
+    }
     list_free(mylist);
     return 0;
 }
