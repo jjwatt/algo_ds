1d1d669bd5d29ddc8e48017c242e5241a89d1b53
Jesse Wattenbarger <jwatt@broken.watch>
2025-11-25 19:09:47 -0500

    Add is_anagram

--- leetcode/scratch.py
+++ leetcode/scratch.py
@@ -152,6 +152,15 @@ def decode(s: str) -> list[str]:
         i = j
     return res
 
+def is_anagram(s: str, t: str) -> bool:
+    s_dict = {}
+    t_dict = {}
+    for i in s:
+        s_dict[i] = 1 + s_dict.get(i, 0)
+    for j in t:
+        t_dict[j] = 1 + t_dict.get(j, 0)
+    return s_dict == t_dict
+
 
 if __name__ == "__main__":
     lst = [1, 2, 3, 4]
