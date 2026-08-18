1fb5df680c448915f91bc8fabd7b9a26866fb19d
Jesse Wattenbarger <jwatt@broken.watch>
2026-08-18 16:14:23 -0400

    More typing in insertion_sort.py

--- python/myalgs/insertion_sort.py
+++ python/myalgs/insertion_sort.py
@@ -2,52 +2,62 @@ from collections.abc import MutableSequence, Sequence
 from typing import Protocol, Self
 
 
+class SupportsLessThan(Protocol):
+    def __lt__(self, other: Self, /) -> bool: ...
+
+
 class SupportsLessThanOrEqual(Protocol):
     def __le__(self, other: Self, /) -> bool: ...
 
 
-def insertion_sort1(collection: MutableSequence) -> MutableSequence:
+class SupportsIndexSwap[T](Protocol):
+    def __getitem__(self, index: int, /) -> T: ...
+    def __setitem__(self, index: int, value: T, /) -> None: ...
+
+
+class SupportsIndexableSort[T](Protocol):
+    def __len__(self) -> int: ...
+    def __getitem__(self, index: int, /) -> T: ...
+    def __setitem__(self, index: int, value: T, /) -> None: ...
+
+
+def insertion_sort1[T: SupportsLessThan](collection: SupportsIndexableSort[T]) -> None:
     """Insertion Sort from TheAlgorithms."""
     for loop_index in range(1, len(collection)):
         insertion_index = loop_index
-        while (insertion_index > 0
-               and collection[insertion_index - 1] > collection[insertion_index]):
+        while (0 < insertion_index
+               and  collection[insertion_index] < collection[insertion_index - 1]):
             collection[insertion_index], collection[insertion_index - 1] = (
                 collection[insertion_index - 1],
                 collection[insertion_index],
             )
             insertion_index = insertion_index - 1
-    return collection
 
 
-def exchange(collection: MutableSequence, a: int, b: int) -> None:
+def exchange[T](collection: SupportsIndexSwap[T], a: int, b: int) -> None:
     """Swap indexes a and b in collection."""
     collection[a], collection[b] = collection[b], collection[a]
 
 
-def dec(num: int) -> int:
-    """Return an int one less than num."""
-    return num - 1
-
-
-def insort(collection: MutableSequence) -> MutableSequence:
+# T has a BOUND (SupportsLessThan) while the container is PARAMETERIZED over T.
+def insort[T: SupportsLessThan](collection: SupportsIndexableSort[T]) -> None:
     for i in range(1, len(collection)):
         j = i
-        while (j > 0 and collection[j - 1] > collection[j]):
+        while (0 < j and collection[j] < collection[j - 1]):
             exchange(collection, j, (j-1))
-            j = dec(j)
-    return collection
+            j -= 1
 
-def insort_c(collection: MutableSequence) -> MutableSequence:
+
+def insort_c[T: SupportsLessThan](collection: SupportsIndexableSort[T]) -> None:
     """Insertion sort ported from C."""
-    for i, _ in enumerate(collection):
+    for i in range(1, len(collection)):
         j = i - 1
         key = collection[i]
-        while (j >= 0 and key < collection[j]):
+        while (0 <= j and key < collection[j]):
             collection[j + 1] = collection[j]
             j = j - 1
         collection[j + 1] = key
-    return collection
+
 
 def insort_w(A: MutableSequence) -> MutableSequence:
     """Insertion sort from wikipedia and similar to the one from C.
@@ -75,6 +85,8 @@ def finsert[T: SupportsLessThanOrEqual](lst: tuple[T, ...], x: T) -> tuple[T, ..
             return (x, y, *ys)
         case (y, *ys):
             return (y, *finsert(tuple(ys), x))
+        case _:
+            raise AssertionError("Unreachable")
 
 
 def finsertion_sort[T: SupportsLessThanOrEqual](seq: Sequence[T]) -> tuple[T, ...]:
@@ -84,5 +96,7 @@ def finsertion_sort[T: SupportsLessThanOrEqual](seq: Sequence[T]) -> tuple[T, ..
             return ()
         case (x, *xs):
             return finsert(finsertion_sort(tuple(xs)), x)
+        case _:
+            raise AssertionError("Unreachable")
 
 
