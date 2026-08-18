bce2ebcd297a002e4dae4bb1a6b6b8c113274536
Jesse Wattenbarger <jwatt@broken.watch>
2026-08-18 07:53:40 -0400

    Add first shot at a flake.nix for different devShells

---
+++ flake.nix
@@ -0,0 +1,117 @@
+{
+  description = "Polyglot algorithms and data structures study monorepo";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs };
+  let
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+
+    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f (import nixpkgs { inherit system; }));
+  in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ git ];
+        };
+
+        c = pkgs.mkShell {
+          packages = with pkgs; [
+            gcc
+            gnumake
+            gdb
+            valgrind
+            clang-tools
+          ];
+        };
+
+        cc = pkgs.mkShell {
+          packages = with pkgs; [
+            gcc
+            gnumake
+            cmake
+            gdb
+            valgrind
+            clang-tools
+            # TODO: Add bazel
+          ];
+        };
+
+        python = pkgs.mkShell {
+          packages = with pkgs; [
+            uv
+            python315
+            pkg-config
+            stdenv.cc
+          ];
+          env = {
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
+              stdenv.cc.cc.lib
+              zlib
+            ]);
+          };
+        };
+
+        scheme = pkgs.mkShell {
+          packages = with pkgs; [
+            guile
+            chez
+            racket
+          ];
+        };
+
+        ocaml = pkgs.mkShell {
+          packages = with pkgs; [
+            ocaml
+            dune_3
+            ocamlPackages.findlib
+            ocamlPackages.ocamlformat
+            ocamlPackages.utop
+            ocamlPackages.ocaml-lsp
+          ];
+        };
+
+        haskell = pkgs.mkShell {
+          packages = with pkgs; [
+            ghc
+            cabal-install
+            haskell-language-server
+            hlint
+          ];
+        };
+
+        erlang = pkgs.mkShell {
+          packages = with pkgs; [
+            erlang
+            rebar3
+            erlang-ls
+          ];
+        };
+
+        javascript = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs
+            corepack
+            typescript
+            typescript-language-server
+          ];
+        };
+
+        bash = pkgs.mkShell {
+          packages = with pkgs; [
+            bashInteractive
+            shellcheck
+            shfmt
+          ];
+        };
+
+      });
+    };
+}
